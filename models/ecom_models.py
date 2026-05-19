d7175942dcee332cfd376c60eab5593f4dff302f
Oleksandr Piatnytsia <a_pyatnytsia@viyar.ua>
2026-05-19 21:40:15 +0300

    product class added

---
+++ models/ecom_models.py
@@ -0,0 +1,58 @@
+from enum import Enum
+
+
+class ProductCategory(str, Enum):
+    soft_toy = "soft toy"
+    constructor = "constructor"
+    wear = "wear"
+    paw_patrol = "paw patrol"
+
+
+class Currency(str, Enum):
+    USD = "USD"
+    EUR = "EUR"
+    UAH = "UAH"
+    PLN = "PLN"
+
+
+class Product:
+    title: str
+    category: ProductCategory
+    currency: Currency
+    description: str | None
+    stock_quantity: int
+    image: str
+    __price: int  # price in coins
+    __stock_quantity: list[int]
+
+    def __init__(
+        self,
+        title: str,
+        category: ProductCategory,
+        currency: Currency,
+        description: str | None = None,
+    ) -> None:
+        self.title = title
+        self.category = category
+        self.currency = currency
+        self.description = description
+
+    def __str__(self):
+        return f"{self.title} ({self.price})"
+
+    def __repr__(self):
+        return f"{self.title} ({self.price})"
+
+    @property
+    def price(self) -> int:
+        return self.__price
+
+    @property
+    def stock_quantity(self) -> int:
+        return sum(self.__stock_quantity)
+
+    def update_stock(self, quantity: int) -> None:
+        self.__stock_quantity.append(quantity)
+
+    def change_price(self, new_price: int) -> None:
+        self.__price = new_price
